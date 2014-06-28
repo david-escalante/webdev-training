af15a556bde0cf0a97de43867e8d7fe13e643349
Maikol Guzman <mikeguzman@gmail.com>
2014-06-28 13:43:30 -0600

    step-0
    Demo of using directives with AngularJS
    
    Signed-off-by: Maikol Guzman <mikeguzman@gmail.com>

---
+++ script.js
@@ -0,0 +1,9 @@
+/**
+ * Created by mike-mac on 6/28/14.
+ */
+
+var demoApp = angular.module('demoApp', []);
+
+demoApp.controller('HelloController', function ($scope) {
+   alert("Hi Mike From Avantica");
+});
