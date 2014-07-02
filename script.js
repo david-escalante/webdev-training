35e839263f03c054133831fde147f59ca0a091e6
Maikol Guzman <mikeguzman@gmail.com>
2014-07-02 16:51:19 -0600

    step-jquery

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
  * Created by mike-mac on 6/28/14.
  */
 
-var demoApp = angular.module('demoApp', []);
-
-demoApp.controller('HelloController', function ($scope) {
-   alert("Hi Mike From Avantica");
+$(document).ready(function() {
+    $( "#txtName" ).keypress(function() {
+            $("#text").text($(this).val());
+    });
 });
