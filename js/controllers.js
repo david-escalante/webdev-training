f014dcad9480f81fedbbffceb8bb736c8493a918
Maikol Guzman <mikeguzman@gmail.com>
2014-07-02 16:15:13 -0600

    step-mvc-0
    Added a new page with frameworks

--- js/controllers.js
+++ js/controllers.js
@@ -4,7 +4,7 @@
 
 var demoApp = angular.module('demoApp', []);
 
-demoApp.controller('DemoListCtrl', function ($scope) {
+demoApp.controller('IndexController', function ($scope) {
     $scope.title = 'Example MVC - Basic Demo';
     $scope.sub_title = 'JavaScript MV* Patterns';
     $scope.avantica= 'Avantica Technologies';
@@ -15,5 +15,19 @@ demoApp.controller('DemoListCtrl', function ($scope) {
             'snippet': 'Model-view-presenter (MVP) is a derivative of the MVC design pattern which focuses on improving presentation logic. It originated at a company named Taligent in the early 1990s while they were working on a model for a C++ CommonPoint environment.'},
         {'name': 'MVVM',
             'snippet': 'MVVM (Model View ViewModel) is an architectural pattern based on MVC and MVP, which attempts to more clearly separate the development of user-interfaces (UI) from that of the business logic and behavior in an application.'}
+    ];
+});
+
+demoApp.controller('InnerController', function ($scope) {
+    $scope.title = 'Example MVC - Basic Demo - Inner Page';
+    $scope.sub_title = 'Frameworks';
+    $scope.avantica= 'Avantica Technologies';
+    $scope.frameworks = [
+        {'name': 'jQuery',
+            'snippet': 'jQuery is a cross-platform JavaScript library designed to simplify the client-side scripting of HTML.'},
+        {'name': 'Bootstrap',
+            'snippet': 'Bootstrap is a free collection of tools for creating websites and web applications.'},
+        {'name': 'node.js',
+            'snippet': 'Node.js is a software platform for scalable server-side and networking applications.'}
     ];
 });
